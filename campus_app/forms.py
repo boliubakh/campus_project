58d951558ac759acec8b811dcf0dcb6190142ee5
boliubakh <boliubakh_do@knuba.edu.ua>
2025-06-16 19:53:00 +0300

    initial commit

---
+++ campus_app/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import forms
+from django.forms import ModelForm
+from .models import Article
+
+
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['type', 'name', 'description', 'file']
+        widgets = {
+            'description': forms.Textarea(attrs={'placeholder': 'Опис', 'class': 'form-control', 'rows': '5'}),
+            'type': forms.Select(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'file': forms.FileInput(),
+        }
